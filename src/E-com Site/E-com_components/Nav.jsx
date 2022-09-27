6e8070747b03b8d6942b95eb000e39f269545b23
Himanshu <Himanshu@gmail.com>
2022-09-27 10:42:28 +0530

    Second Message

--- src/E-com Site/E-com_components/Nav.jsx
+++ src/E-com Site/E-com_components/Nav.jsx
@@ -6,13 +6,16 @@ import { NavLink } from 'react-router-dom';
 const Nav = ()=> {
 
     return (<>
-0
-<div className='divi  flex'>
+
+<div className=' flex'>
 
     <ul className='list hidden md:flex items-center gap-9 backdrop-blur-lg z-10 bg-[#ffffff14] md:static fixed top-0 right-0 h-screen md:h-auto'>
         <li><NavLink className=" hover:text-blue-700 md:inline-block hover:underline border-transparent duration-300" to="/">Home</NavLink></li>
    
         <li><NavLink className=" hover:text-blue-700 md:inline-block hover:underline border-transparent duration-300" to="/About">About</NavLink></li>
+
+
+
 
         <li><NavLink className=" hover:text-blue-700 md:inline-block hover:underline border-transparent duration-300" to="/Products">Product</NavLink></li>
    
